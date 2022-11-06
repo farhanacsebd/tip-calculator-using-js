1a0f0fba7663b22f5e16979ed9b58eb74bbf2633
farhanacsebd <farhana.cse.bd@gmail.com>
2022-11-06 23:41:55 +0600

    id handle in html

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
-// alert('hello')
+const totalAmount = parseFloat(document.getElementById('total-amount').value); 
+const service = parseFloat(document.getElementById('service').value); 
+const totalPeople = parseFloat(document.getElementById('total-people').value); 
+
+document.getElementById('btn').addEventListener('click',function(){
+    
+})
