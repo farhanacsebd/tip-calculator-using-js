8d4da3132c74ba353e1d770aaaff6ac752756833
farhanacsebd <farhana.cse.bd@gmail.com>
2022-11-07 00:14:29 +0600

    total calculation done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,26 @@
-const totalAmount = parseFloat(document.getElementById('total-amount').value); 
-const service = parseFloat(document.getElementById('service').value); 
-const totalPeople = parseFloat(document.getElementById('total-people').value); 
-
 document.getElementById('btn').addEventListener('click',function(){
     
+    let totalAmount = document.getElementById('total-amount').value; 
+    let service = document.getElementById('service').value; 
+    let totalPeople = document.getElementById('total-people').value; 
+
+    if(totalAmount <= 0){
+        alert('Please enter value')
+        return;
+    }
+
+    else if(totalPeople <= 0){
+        alert('Please enter value')
+        return;
+    }
+
+    else if(totalAmount === "" && service === 0 && totalPeople <= 0){
+        alert('Please enter value')
+        return;
+    }
+
+    let total = (totalAmount * service) / totalPeople;
+    total = total.toFixed(2)
+    
+    document.getElementById('per-tip').innerHTML = total;
 })
